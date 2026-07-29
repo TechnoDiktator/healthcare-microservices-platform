3539e8a502755d3692daf1c3c73725ad12e192be
TechnoDiktator <rastogitarang5@gmail.com>
2026-07-29 14:40:07 +0530

    getting error 403 in billing calls  ... adding logs for bug fix

--- patient-service/src/main/java/com/pm/patientservice/controller/BillController.java
+++ patient-service/src/main/java/com/pm/patientservice/controller/BillController.java
@@ -5,8 +5,9 @@ import com.pm.patientservice.dto.PaymentResponseDTO;
 import com.pm.patientservice.security.AuthorizationService;
 import com.pm.patientservice.security.UserContext;
 import com.pm.patientservice.service.BillService;
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
@@ -17,6 +18,9 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/patients")
 @Tag(name = "Billing", description = "Patient Billing APIs")
 public class BillController {
+
+    private static final Logger log =
+            LoggerFactory.getLogger(BillController.class);
 
     private final BillService billService;
     private final AuthorizationService authorizationService;
@@ -37,12 +41,32 @@ public class BillController {
 
         UserContext user = getCurrentUser();
 
-        authorizationService.authorizePatientBillAccess(
-                user,
-                patientId);
+        log.info(
+                "Get Bill request received. userId={}, email={}, role={}, patientId={}, billId={}",
+                user.getUserId(),
+                user.getEmail(),
+                user.getRole(),
+                patientId,
+                billId
+        );
 
-        return ResponseEntity.ok(
-                billService.getBill(patientId, billId));
+        authorizationService.authorizePatientBillAccess(user, patientId);
+
+        log.info(
+                "Authorization successful for user {} to access bill {}",
+                user.getEmail(),
+                billId
+        );
+
+        BillResponseDTO response = billService.getBill(patientId, billId);
+
+        log.info(
+                "Successfully fetched bill {} for patient {}",
+                billId,
+                patientId
+        );
+
+        return ResponseEntity.ok(response);
     }
 
     @PutMapping("/{patientId}/bills/{billId}/pay")
@@ -52,16 +76,42 @@ public class BillController {
             @PathVariable String billId) {
 
         UserContext user = getCurrentUser();
+
+        log.info(
+                "Pay Bill request received. userId={}, email={}, role={}, patientId={}, billId={}",
+                user.getUserId(),
+                user.getEmail(),
+                user.getRole(),
+                patientId,
+                billId
+        );
+
         authorizationService.authorizePayBill(user, patientId);
 
-        return ResponseEntity.ok(
-                billService.payBill(patientId, billId));
+        log.info(
+                "Authorization successful for payment. user={}, billId={}",
+                user.getEmail(),
+                billId
+        );
+
+        PaymentResponseDTO response = billService.payBill(patientId, billId);
+
+        log.info(
+                "Successfully paid bill {} for patient {}",
+                billId,
+                patientId
+        );
+
+        return ResponseEntity.ok(response);
     }
 
     private UserContext getCurrentUser() {
 
         Authentication authentication =
                 SecurityContextHolder.getContext().getAuthentication();
+
+        log.info("Authentication: {}", authentication);
+        log.info("Authorities: {}", authentication.getAuthorities());
 
         return (UserContext) authentication.getPrincipal();
     }
